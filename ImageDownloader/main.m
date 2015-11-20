309ab99fbaccd61246633cf684134b45bd9e545b
Bogdan Evsenev <evsenev.bogdan@gmail.com>
2015-11-20 15:04:50 +0300

    Update main.m

--- ImageDownloader/main.m
+++ ImageDownloader/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  ImageDownloader
-//
-//  Created by appledev on 20/11/15.
-//  Copyright © 2015 BED. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, const char * argv[]) {
